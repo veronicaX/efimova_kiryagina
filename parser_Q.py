65456c2e5ca5ccfb71a29e1ecc1e03971bff8fc9
veronicaX <veronicaefimova@gmail.com>
2017-02-27 18:20:33 +0300

    Update parser_Q.py

--- parser_Q.py
+++ parser_Q.py
@@ -25,7 +25,7 @@ for i in range(range_from, range_to + 1):
     
     
     page = BeautifulSoup(theQ.text, 'html.parser') 
-    link_vk = page.find('a', {'class': 'profile__header-social-icon font-icon theq_social_vk_fill'}) 
+    link_vk = page.find('a', {'class': 'profile__header-social-icon font-icon theq_social_vk_fill'}).get('href')  
     theQ_with_name = page.find('meta', {'property': 'og:url'}).get('content') 
     theQ_with_name = theQ_with_name + str('?filter=followed') 
     theQ_with_name = requests.get(theQ_with_name) 
