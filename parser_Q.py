044f4fc16afc26496857cb2e4ac7850837bf138a
veronicaX <veronicaefimova@gmail.com>
2017-02-28 18:47:41 +0300

    Update parser_Q.py
    всё, исправила

--- parser_Q.py
+++ parser_Q.py
@@ -2,7 +2,7 @@
 '''
     import sys, os 
     os.getcwd() #узнать, какая сейчас директория
-    os.chdir('C:\\......')
+    os.chdir('C:\\Users\\user.SPB\\Desktop')
     
 '''
 import requests 
@@ -16,7 +16,7 @@ import datetime
 begin = str(datetime.datetime.now())
 
 range_from = 1
-range_to = 4000
+range_to = 20
 for i in range(range_from, range_to + 1): 
     theQ = str('https://thequestion.ru/account/') 
     theQ = theQ + str(i) 
@@ -25,30 +25,28 @@ for i in range(range_from, range_to + 1):
     
     
     page = BeautifulSoup(theQ.text, 'html.parser') 
-    link_vk = page.find('a', {'class': 'profile__header-social-icon font-icon theq_social_vk_fill'}).get('href')  
-    theQ_with_name = page.find('meta', {'property': 'og:url'}).get('content') 
-    theQ_with_name = theQ_with_name + str('?filter=followed') 
-    theQ_with_name = requests.get(theQ_with_name) 
-    theQ_with_name = BeautifulSoup(theQ_with_name.text, 'html.parser') 
-    followed = theQ_with_name.find_all('div', {'class': 'list-questions__header-center'})   
-    if followed is None: 
-        followed = "NA"
+    link_vk = page.find('a', {'class': 'profile__header-social-icon font-icon theq_social_vk_fill'}) 
+
+    if link_vk is None: 
+        link_vk = "NA" 
+        followed = "NA" 
     else: 
-        followed = "1"
-        
-    df.loc[len(df)]=[str(i), link_vk, followed]
+        link_vk = page.find('a', {'class': 'profile__header-social-icon font-icon theq_social_vk_fill'}).get('href') 
+        link_vk = link_vk.replace("http://vk.com/id", "") 
+        theQ_with_name = page.find('meta', {'property': 'og:url'}).get('content') 
+        theQ_with_name = theQ_with_name + str('?filter=followed') 
+        theQ_with_name = requests.get(theQ_with_name) 
+        theQ_with_name = BeautifulSoup(theQ_with_name.text, 'html.parser') 
+        followed = theQ_with_name.find('div', {'class': 'list-questions__header-center'}) 
+    if followed is None: 
+            followed = "0" 
+    else: 
+            followed = "1" 
+    df.loc[len(df)]=[str(i), link_vk, followed] 
+    end = str(datetime.datetime.now())
     
-    if i % 1000 == 0: #сохраняем через каждую тысячу
+    if i % 10000 == 0: #сохраняем через каждую тысячу
         df.to_csv('dataset_' + str(range_from) + '-' + str(i) +'.csv')
         
 df.to_csv('dataset_' + str(range_from) + '-' + str(range_to) +'.csv')  #сохранится в директорию
 end = str(datetime.datetime.now()) 
-
-
-
-
-
-
-
-
-
